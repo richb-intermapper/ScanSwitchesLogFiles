20ea949f90e30363b9a5c4a25a28fe45a7b44e8a
Rich Brown <richb.hanover@gmail.com>
2013-06-01 16:31:36 -0400

    Created L2LogFile, L2HistoryBuffer, L2ScanTables classes. Output is identical to previous version of the program.

--- scanl2logs.py
+++ scanl2logs.py
@@ -23,39 +23,14 @@ import socket
 import struct
 import argparse
 
-tablelist = []                              # a list of dictionaries - one for each table (9 tables/poller)
-labeldict = {}                              # names (actually icon labels), indexed by imid
-adrsdict = {}                               # IP addresses, indexed by imid
-syssvcdict = {}                             # and the system services
 
-syssvclookup = {
-    '1': "Hub",
-    '2': "Sw",
-    '4': "Rtr",
-    '6': "L3 Sw",
-    '7': "L3 SwHub",
-    '64': "Host (64)",
-    '72': "Host (72)",
-    '76': "Host (76)",
-    '78': "Host (78)"
-}
+thelog = None                               # the log file to be processed
+theHistory = None                           # a long string that contains interesting log file lines
+theScanInfo = None                          # Detailed L2 scan info - results of scanning each table
 
-thelog = None                               # Globals
-theHistory = None
-
-def initGlobals():
+class L2LogFile:
     '''
-    Clear out all the global variables so they can be used for next harvest
-    '''
-    global tablelist, labeldict, adrsdict, syssvcdict
-    tablelist = []                              # a list of dictionaries - one for each table (9 tables/poller)
-    labeldict = {}                              # names (actually icon labels), indexed by imid
-    adrsdict = {}                               # IP addresses, indexed by imid
-    syssvcdict = {}                             # and the system services
-
-class L2Log:
-    '''
-    Class to return a line from the log file. Concatenates lines that begin with white space,
+    Handle an InterMapper Layer2 log file. Returns one line at a time, concatenating lines that begin with white space,
         and keeps track of the "real" line number (e.g., the first)
     '''
 
@@ -91,107 +66,7 @@ class L2Log:
         return retline
 
 
-def processOpentable(line):
-    pos = line.find('id=')
-    line = line.replace("id=","kcid=")      # change "id" to "kcid" on <KC_opentable
-    t = line[0:19]
-    kvs = {k:v.strip("'") for k,v in re.findall(r'(\S+)=(".*?"|\S+)', line)}
-
-    tbl = {}                            # fill in all possible fields
-    tbl['startTime'] = t
-    tbl['endTime'] = "                   "  # 19 spaces
-    tbl['startLine'] = thelog.linenum
-    tbl['endLine'] = "-"
-    tbl['imid'] = kvs['target']
-    tbl['kcid'] = kvs['kcid']
-    tbl['tableName'] = '-'
-    tbl['tableid'] = "-"
-    tbl['rows'] = 0
-    tablelist.append(tbl)
-
-    # ensure IMID is in other data structures
-    imid = kvs['target']
-    labeldict.setdefault(imid, "-")
-    syssvcdict.setdefault(imid, "")
-    adrsdict.setdefault(imid, "0")
-
-
-def processKR(line):
-    line = line.replace('KR id','KR kcid')   # change "id" to "kcid" on <KR id...
-    kvs = {k:v.strip("'") for k,v in re.findall(r'(\S+)=(".*?"|\S+)', line)}
-    if kvs['kcid'] == "274":
-        pass
-
-    for l in tablelist:
-        if l['kcid'] == kvs['kcid']:
-            l['kcid'] += "+"                # mark the KCid as having received a KR
-            l['tableid'] = kvs['id']        # record the corresponding id= for the table
-            l['tableName'] = kvs['title']   # and the table name
-            l['rows'] = 0                   # zero out the row count
-            break
-
-
-def processTableData(line):
-    t = line[0:19]                          # get the time stamp from the line
-    kvs = {k:v.strip("'") for k,v in re.findall(r'(\S+)=(".*?"|\S+)', line)}
-
-    for l in tablelist:
-        if l['tableid'] == kvs['id']:       # found matching table entry
-            if 'done' in kvs:
-                l['tableid'] += " "+kvs['done'] # mark it so it no longer matches
-                l['endTime'] = t            # save the end time
-                l['endLine'] = thelog.linenum
-            else:
-                l['rows'] += 1              # otherwise, bump the row count
-            break                           # and exit
-    pass
-
-
-def processSQLline(line):
-    pos = line.find('VALUES (')
-    part = line[pos+len('VALUES ('):]
-    ary = part.split(",")
-    imid = ary[0].strip("'")
-    label = ary[1].strip("' ")
-    ip = ary[2].strip("'x ")
-    svcs = ary[6].strip("' ")
-    svc = syssvclookup.get(svcs, str(svcs))
-
-    syssvcdict[imid] = svc
-    labeldict[imid] = label
-    adrsdict[imid] = ip
-
-
-def printTables(fo):
-
-    fo.write(theHistory.gLineBuffer+"\n")
-    fo.write("Resulting Table Info\n")
-    fo.write("%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s\n" % ("Start Time", "End Time", "Start Line", "End Line","IMID", "HexIP", "KCid", "TableID", "IP", "Label", "SysSvc", "TableName", "Rows", "Diag"))
-    for l in tablelist:
-        st = l['startTime']
-        et = l['endTime']
-        sl = l['startLine']
-        el = l['endLine']
-        imid = l['imid']
-        kcid = l['kcid']
-        tn = l['tableName']
-        tid = l['tableid']
-        r = l['rows']
-        lbl = labeldict[imid]
-        svc = syssvcdict[imid]
-        hexip = adrsdict[imid]
-        addr_long = int(hexip,16)
-        ip = socket.inet_ntoa(struct.pack(">L", addr_long))
-        diag = ""
-        if not "+" in kcid:
-            diag += "Never received matching 'KR'; "
-        if "No" in tid:
-            diag += "Received NoAnswer; "
-        elif not "Pa" in tid and r != 0:
-            diag += "Never received end of table data; "
-        fo.write(" %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s\n" % (st, et, sl, el, imid, hexip, kcid, tid, ip, lbl, svc, tn, r, diag))
-
-class HistoryBuffer():
+class L2HistoryBuffer():
     '''
     Collect lines of interest into a global buffer for displaying as history
     '''
@@ -264,21 +139,146 @@ class HistoryBuffer():
             self.prevtime = line[0:19]
             self.dupcount = 0
 
-def processLine(line):
+
+class L2ScanTables:
     '''
-    Process each line of the file, triggering on interesting states/info
+    Saves the detailed information about the various table scan/harvest.
     '''
-    theHistory.logit(line)
-    if 'KALI: <' in line:
-        line = line.replace(">"," >")
-        if '<KC_opentable' in line:
-            processOpentable(line)
-        elif 'KR id' in line:
-            processKR(line)
-        elif '<KU_tabledata' in line:
-            processTableData(line)
-    elif 'SQL #-1: INSERT INTO device' in line:
-        processSQLline(line)
+    def __init__(self):
+        '''
+        Clear out all the global variables so they can be used for next harvest
+        '''
+        self.tablelist = []                              # a list of dictionaries - one for each table (9 tables/poller)
+        self.labeldict = {}                              # names (actually icon labels), indexed by imid
+        self.adrsdict = {}                               # IP addresses, indexed by imid
+        self.syssvcdict = {}                             # and the system services
+
+        self.syssvclookup = {
+            '1': "Hub",
+            '2': "Sw",
+            '4': "Rtr",
+            '6': "L3 Sw",
+            '7': "L3 SwHub",
+            '64': "Host (64)",
+            '72': "Host (72)",
+            '76': "Host (76)",
+            '78': "Host (78)"
+        }
+
+
+    def processOpentable(self, line):
+        # pos = line.find('id=')
+        line = line.replace("id=","kcid=")      # change "id" to "kcid" on <KC_opentable
+        t = line[0:19]
+        kvs = {k:v.strip("'") for k,v in re.findall(r'(\S+)=(".*?"|\S+)', line)}
+
+        tbl = {}                            # fill in all possible fields
+        tbl['startTime'] = t
+        tbl['endTime'] = "                   "  # 19 spaces
+        tbl['startLine'] = thelog.linenum
+        tbl['endLine'] = "-"
+        tbl['imid'] = kvs['target']
+        tbl['kcid'] = kvs['kcid']
+        tbl['tableName'] = '-'
+        tbl['tableid'] = "-"
+        tbl['rows'] = 0
+        self.tablelist.append(tbl)
+
+        # ensure IMID is in other data structures
+        imid = kvs['target']
+        self.labeldict.setdefault(imid, "-")
+        self.syssvcdict.setdefault(imid, "")
+        self.adrsdict.setdefault(imid, "0")
+
+
+    def processKR(self, line):
+        line = line.replace('KR id','KR kcid')   # change "id" to "kcid" on <KR id...
+        kvs = {k:v.strip("'") for k,v in re.findall(r'(\S+)=(".*?"|\S+)', line)}
+        if kvs['kcid'] == "274":
+            pass
+
+        for l in self.tablelist:
+            if l['kcid'] == kvs['kcid']:
+                l['kcid'] += "+"                # mark the KCid as having received a KR
+                l['tableid'] = kvs['id']        # record the corresponding id= for the table
+                l['tableName'] = kvs['title']   # and the table name
+                l['rows'] = 0                   # zero out the row count
+                break
+
+
+    def processTableData(self, line):
+        t = line[0:19]                          # get the time stamp from the line
+        kvs = {k:v.strip("'") for k,v in re.findall(r'(\S+)=(".*?"|\S+)', line)}
+
+        for l in self.tablelist:
+            if l['tableid'] == kvs['id']:       # found matching table entry
+                if 'done' in kvs:
+                    l['tableid'] += " "+kvs['done'] # mark it so it no longer matches
+                    l['endTime'] = t            # save the end time
+                    l['endLine'] = thelog.linenum
+                else:
+                    l['rows'] += 1              # otherwise, bump the row count
+                break                           # and exit
+        pass
+
+
+    def processSQLline(self, line):
+        pos = line.find('VALUES (')
+        part = line[pos+len('VALUES ('):]
+        ary = part.split(",")
+        imid = ary[0].strip("'")
+        label = ary[1].strip("' ")
+        ip = ary[2].strip("'x ")
+        svcs = ary[6].strip("' ")
+        svc = self.syssvclookup.get(svcs, str(svcs))
+
+        self.syssvcdict[imid] = svc
+        self.labeldict[imid] = label
+        self.adrsdict[imid] = ip
+
+
+    def printL2Tables(self):
+
+        retline = ("%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s\n" % ("Start Time", "End Time", "Start Line", "End Line","IMID", "HexIP", "KCid", "TableID", "IP", "Label", "SysSvc", "TableName", "Rows", "Diag"))
+        for l in self.tablelist:
+            st = l['startTime']
+            et = l['endTime']
+            sl = l['startLine']
+            el = l['endLine']
+            imid = l['imid']
+            kcid = l['kcid']
+            tn = l['tableName']
+            tid = l['tableid']
+            r = l['rows']
+            lbl = self.labeldict[imid]
+            svc = self.syssvcdict[imid]
+            hexip = self.adrsdict[imid]
+            addr_long = int(hexip,16)
+            ip = socket.inet_ntoa(struct.pack(">L", addr_long))
+            diag = ""
+            if not "+" in kcid:
+                diag += "Never received matching 'KR'; "
+            if "No" in tid:
+                diag += "Received NoAnswer; "
+            elif not "Pa" in tid and r != 0:
+                diag += "Never received end of table data; "
+            retline += " %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s\n" % (st, et, sl, el, imid, hexip, kcid, tid, ip, lbl, svc, tn, r, diag)
+        return retline
+
+    def processLine(self, line):
+        '''
+        Process each line of the file, triggering on interesting states/info
+        '''
+        if 'KALI: <' in line:
+            line = line.replace(">"," >")
+            if '<KC_opentable' in line:
+                self.processOpentable(line)
+            elif 'KR id' in line:
+                self.processKR(line)
+            elif '<KU_tabledata' in line:
+                self.processTableData(line)
+        elif 'SQL #-1: INSERT INTO device' in line:
+            self.processSQLline(line)
 
 
 def main(argv=None):
@@ -297,18 +297,21 @@ def main(argv=None):
     # Now to the meat of the program
 
     global thelog, theHistory
-    thelog = L2Log(f)                       # initialize the file object
-    theHistory = HistoryBuffer()
+    thelog = L2LogFile(f)                  # initialize the file object
+    theHistory = L2HistoryBuffer()
+    theScanInfo = L2ScanTables()
 
     while True:
         line = thelog.getline()
         if line == "":
             break
-        processLine(line)
+        theHistory.logit(line)
+        theScanInfo.processLine(line)
 
     fo.write("Reading switches.log, %s\n" % os.path.abspath(f.name))
-
-    printTables(fo)                         # print the relevant information
+    fo.write(theHistory.gLineBuffer+"\n")
+    fo.write("Resulting Table Info\n")
+    fo.write(theScanInfo.printL2Tables())                         # print the relevant information
 
 if __name__ == "__main__":
     sys.exit(main())
